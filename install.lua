c3bc2ac36744a677d5bdb7500f0801717dd0f408
Smoti <81593246+Smotify@users.noreply.github.com>
2023-04-07 14:30:13 +0200

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,28 @@
+local currentVersion = "1.0" -- tutaj wprowadź aktualną wersję twojego skryptu
+local githubUrl = "https://raw.githubusercontent.com/Smotify/skrypty/main/skrypt.lua" -- tutaj wprowadź adres URL Twojego pliku skryptu na GitHubie
+
+local function download(url, path)
+    local content = http.get(url).readAll()
+    local file = fs.open(path, "w")
+    file.write(content)
+    file.close()
+end
+
+local function getVersion(url)
+    local content = http.get(url).readAll()
+    local version = content:match('local currentVersion = "(.-)"')
+    return version
+end
+
+local function install(url)
+    download(url, "skrypt.lua")
+    print("Pobrano nowszą wersję skryptu!")
+end
+
+local githubVersion = getVersion(githubUrl)
+
+if currentVersion < githubVersion then
+    install(githubUrl)
+else
+    print("Masz już najnowszą wersję skryptu!")
+end
